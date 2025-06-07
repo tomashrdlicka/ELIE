b322e4df72efa9daf60ae5565ad432d5270f3f04
niksirbi <niko.sirbiladze@gmail.com>
2025-06-07 16:26:28 +0100

    configure app to run on replit

--- elie/app.py
+++ elie/app.py
@@ -1,8 +1,9 @@
-# Generate the skeleton of a basiv dash plotly app
-# The app should have a single button, which when clicked prints "Hello World" to the console
+import os
+
 import dash
 from dash import html, dcc, Input, Output
 import dash_bootstrap_components as dbc
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
@@ -24,3 +25,6 @@ def update_output(n_clicks):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # bind to 0.0.0.0 on the Replit-assigned port (defaults to 3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
